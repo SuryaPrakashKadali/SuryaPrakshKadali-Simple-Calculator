04dc3e3c809c36b32cbaf92af4f83d9b2f15a528
SuryaPrakashKadali <41120816+SuryaPrakashKadali@users.noreply.github.com>
2020-02-10 15:59:41 +0530

    First Commit

---
+++ SimpleCalculator/src/main/java/Calculator/SimpleCalculator/Divide.java
@@ -0,0 +1,25 @@
+package Calculator.SimpleCalculator;
+
+class Divide{
+    double a,b;
+    Divide(double a,double b){
+        this.a=a;
+        this.b=b;
+    }
+    void divi(){
+        try {
+            if(this.b==0){
+                throw new ArithmeticException();
+            }
+            else{
+                System.out.println(this.a/this.b);
+            }
+            
+         }
+         catch (ArithmeticException e) { 
+            
+            System.out.println("Enter valid number other than 0");
+         }
+        //System.out.println(this.a/this.b);
+    }
+}
